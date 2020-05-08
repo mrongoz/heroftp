2d5ba64953ec703f621886f25932df834ff2492f
mrongoz <andremrngz@gmail.com>
2020-05-08 10:59:14 +0800

    up

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
-	echo -e "[ftp]\n$RCLONE_CONFIG" > rclone.conf
+	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
 	echo "on-download-stop=./delete.sh" >> aria2c.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
 	chmod +x delete.sh
